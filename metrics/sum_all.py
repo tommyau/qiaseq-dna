3b47475d7d5001c90ed9881906372fc965e102ca
rpadmanabhan <raghus606@gmail.com>
2019-09-03 12:34:48 +0000

    Fix summary aggregation using detail file for IonReads

--- metrics/sum_all.py
+++ metrics/sum_all.py
@@ -8,10 +8,10 @@ def run(cfg):
     duplex     = "duplex"
     prep       = "prep"
     if cfg.outputDetail: # more details if needed
-        prep       = "prep.detail"
+        prep       = "prep.detail" if os.path.exists(readSet + ".prep.detail.summary.txt") else "prep" # ion-reads don't have prep.detail file
         umi_filter = "umi_filter.detail"
         duplex     = "duplex.detail"
-        
+
     # concatenate summary files
     with open(readSet + ".sum_all.summary.txt", "w") as OUT:
         for fileType in (prep, "align", umi_filter, "umi_frags", "sum.uniformity.primer", "umi_depths", duplex, "smCounter", "vcf_complex"):
